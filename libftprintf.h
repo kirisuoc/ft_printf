e8a55ca8fa23c0e209ea6a4aaeb7cd300d770466
kirisuoc <erikcousillas@icloud.com>
2024-09-28 19:12:24 +0200

    Fix prototype in libftprintf.h

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:13:59 by ecousill          #+#    #+#             */
-/*   Updated: 2024/09/28 11:10:22 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/09/28 19:11:21 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-int	ft_printf(char const *, ...);
-int ft_putchar(char c);
+int	ft_printf(char const *format, ...);
+int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(int n);
 
