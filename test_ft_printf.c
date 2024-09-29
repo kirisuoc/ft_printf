8baebe727145690f336ecc3d7759add49b2866a7
kirisuoc <erikcousillas@icloud.com>
2024-09-29 13:37:48 +0200

    Tests ft_printf.c

--- test_ft_printf.c
+++ test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 15:30:21 by ecousill          #+#    #+#             */
-/*   Updated: 2024/09/28 19:04:57 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/09/29 13:34:56 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,19 @@ int	main(void)
 	int		number = 56;
 	int		countletters = 0;
 
-	printf("Esto es un caracter: %c\n", character);
+/* 	printf("Esto es un caracter: %c\n", character);
 	printf("Esto es un string: %s\n", string);
 	printf("Esto es un número decimal: %d\n", number);
 	printf("Esto es un entero en base 10: %i\n", number);
 
-	countletters = ft_printf("Hola, %s %i %c\n", string, number, character);
-	printf("Hola, %s %i %c\n", string, number, character);
-	countletters = ft_printf("%20d\n", -21474838);
- 	printf("%d\n", countletters);
+	countletters = ft_printf("Hola, %s %010d %c\n", string, number, character);
+	printf("Hola, %s %i %c\n", string, number, character); */
 
-	printf("%.10d\n", -2138);
-	printf("%010d\n", -2138);
+	int	countletters1 = ft_printf("%015d\n", -2147483647);
+	/* printf("%.10d\n", -2138); */
+	int	countletters2 = printf("%.15d\n", -2147483647);
+
+	ft_printf("%d\n", countletters1);
+	printf("%d\n", countletters2);
 	return (0);
 }
