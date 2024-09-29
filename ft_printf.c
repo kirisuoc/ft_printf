711893ec5ed423d1714c48f52d50ee62d8ca983b
kirisuoc <erikcousillas@icloud.com>
2024-09-29 13:39:29 +0200

    Flag '0' and '-' for number ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 10:57:10 by ecousill          #+#    #+#             */
-/*   Updated: 2024/09/28 19:05:15 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/09/29 13:33:39 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,75 +41,83 @@
 // %X Imprime un número hexadecimal (base 16) en mayúsculas
 // %% para imprimir el símbolo del porcentaje
 
-int	count_digits(int n)
-{
-	int		num_digits;
 
-	if (n == 0)
-		return (1);
-	num_digits = 0;
-	if (n < 0)
-	{
-		if (n == -2147483648)
-		{
-			write(1, "-", 1);
-			return (11);
-		}
-		num_digits++;
-		n = -n;
-		write(1, "-", 1);
-	}
-	while (n > 0)
-	{
-		n /= 10;
-		num_digits++;
-	}
-	return (num_digits);
+int	flag_minus(char **format, long number)
+{
+	int min_width;
+	int	len_number;
+	int	fill_quantity;
+
+	min_width = ft_atoi(*format);
+	len_number = ft_strlen(ft_itoa(number));
+	if (min_width > len_number)
+		len_number = min_width;
+	if (number < 0)
+		write (1, "-", 1);
+	fill_quantity = len_number - ft_putnbr(number);
+	while (fill_quantity-- > 0)
+		write(1, " ", 1);
+	return (len_number);
+}
+int	flag_zero(char **format, long number)
+{
+	int min_width;
+	int	len_number;
+	int	fill_quantity;
+
+	min_width = ft_atoi(*format);
+	len_number = ft_strlen(ft_itoa(number));
+	if (min_width > len_number)
+		len_number = min_width;
+	fill_quantity = min_width - ft_strlen(ft_itoa(number));
+	if (number < 0)
+		write (1, "-", 1);
+	while (fill_quantity-- > 0)
+		write(1, "0", 1);
+	ft_putnbr(number);
+	return (len_number);
 }
 
-void	fill_width(char fill, int quantity)
+int	only_field_min_width(char **format, long number)
 {
-	if (fill == '-')
-		while (quantity-- > 0)
-			write(1, " ", 1);
-	else if (fill == '0')
-		while (quantity-- > 0)
-			write(1, "0", 1);
+	int min_width;
+	int	len_number;
+	int	fill_quantity;
+
+	min_width = ft_atoi(*format);
+	len_number = ft_strlen(ft_itoa(number));
+	if (min_width > len_number)
+		len_number = min_width;
+	fill_quantity = min_width - ft_strlen(ft_itoa(number));
+	while (fill_quantity-- > 0)
+		write(1, " ", 1);
+	if (number < 0)
+		write (1, "-", 1);
+	ft_putnbr(number);
+	return (len_number);
 }
 
-int	manage_conv_number(char **format, int number)
+int	manage_conv_number(char **format, long number)
 {
-	int	width;
+	int	printed_chars;
 	int	fill_quantity;
 
 	if (**format == 'd' || **format == 'i')
 		return (ft_putnbr(number));
+
 	else if (**format == '-')
 	{
 		(*format)++;
-		width = ft_atoi(*format);
-		fill_quantity = width - ft_putnbr(number);
-		while (fill_quantity-- > 0)
-			write(1, " ", 1);
+		printed_chars = flag_minus(format, number);
 	}
 	else if (**format == '0')
 	{
 		(*format)++;
-		width = ft_atoi(*format);
-		fill_quantity = width - count_digits(number);
-		while (fill_quantity-- > 0)
-			write(1, "0", 1);
-		ft_putnbr(number);
+		printed_chars = flag_zero(format, number);
 	}
 	else if (ft_isdigit(**format))
-	{
-		width = ft_atoi(*format);
-		fill_quantity = width - count_digits(number);
-		while (fill_quantity-- > 0)
-			write(1, " ", 1);
-		ft_putnbr(number);
-	}
-	return (width);
+		printed_chars = only_field_min_width(format, number);
+	return (printed_chars);
 }
 
 int	get_conv_type(char *format)
