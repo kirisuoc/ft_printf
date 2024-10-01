3fc79f096b66c047eb268b6b936cd6876b1369a1
kirisuoc <erikcousillas@icloud.com>
2024-10-01 13:32:25 +0200

    Change flag due to change bonus

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:13:59 by ecousill          #+#    #+#             */
-/*   Updated: 2024/09/30 22:32:26 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/01 13:02:42 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@
 
 typedef struct s_format
 {
-	int		flag_minus;
-	int		flag_zero;
+	int		flag_plus;
+	int		flag_hash;
+	int		flag_space;
 	int		min_width;
-	int		precision;
 	char	specifier;
 }			t_format;
 
@@ -34,7 +34,6 @@ int		ft_putchar(char c);
 int		ft_putstr(char *s);
 int		ft_putnbr(int n);
 int		get_number_length(int number);
-int		manage_c(t_format *info, va_list *args);
 int		manage_d(t_format *info, va_list *args);
 void	parse_format(char **format, t_format *info);
 
