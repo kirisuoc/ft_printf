d8f3924c510cd0cf79442df9bc0ab4bc451df0b6
kirisuoc <erikcousillas@icloud.com>
2024-10-01 17:51:37 +0200

    Fix counter ft_putnbr and add ft_putnbr_unsigned

--- put_functions.c
+++ put_functions.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 11:08:55 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/01 13:26:23 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/01 17:49:25 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,29 @@ int	ft_putnbr(int n)
 		if (n == -2147483648)
 		{
 			write (1, "2147483648", 11);
-			return (11);
+			return (10);
 		}
 		n = -n;
 	}
 	if (n >= 10)
 	{
 		count += ft_putnbr(n / 10);
+	}
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+	count++;
+	return (count);
+}
+
+int	ft_putnbr_unsigned(unsigned int n)
+{
+	char	digit;
+	int		count;
+
+	count = 0;
+	if (n >= 10)
+	{
+		count += ft_putnbr_unsigned(n / 10);
 	}
 	digit = n % 10 + '0';
 	write(1, &digit, 1);
